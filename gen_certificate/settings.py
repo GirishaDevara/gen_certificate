2c805d2191869a44c167d15bc29e13ea0087342d
girisha devara <51402153+GirishaDevara@users.noreply.github.com>
2020-07-21 11:24:36 +0530

    Update settings.py

--- gen_certificate/settings.py
+++ gen_certificate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'np7jfwxr&e^d8x*_(^gmy72c!k$obo0#^!73-3+1h=xu8)(dw('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
